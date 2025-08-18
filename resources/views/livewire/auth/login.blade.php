038901c27a340a7bfec0105930df00cb7b4af618
MUHAMMAD QISTI AMALUDDIN BIN MOHD ROZAINI <119896181+QistiAmal1212@users.noreply.github.com>
2025-08-18 15:27:18 +0800

    move only on login need all font black

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -73,6 +73,12 @@ new #[Layout('components.layouts.auth')] class extends Component {
     }
 }; ?>
 
+<style>
+      *{
+    color:black;
+  }
+    </style>
+
 <div class="flex flex-col gap-6 !text-black">
     <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
 
